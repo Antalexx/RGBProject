1f8ef343f543e714465b301a4b6d1843db54533c
Антоненко Александр <aalexanto@gmail.com>
2021-05-23 19:42:50 +0300

    hw 1.2

--- RGBProject/ViewController.swift
+++ RGBProject/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redCount: UILabel!
+    @IBOutlet weak var blueCount: UILabel!
+    @IBOutlet weak var greenCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func changeColor() {
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 
-
+    @IBAction func rSlderChanged(_ sender: UISlider) {
+        redCount.text = String(format: "%.2f", redSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func gSliderChnged(_ sender: UISlider) {
+        greenCount.text = String(format: "%.2f", greenSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func bSliderChanged(_ sender: UISlider) {
+        blueCount.text = String(format: "%.2f", blueSlider.value)
+        changeColor()
+    }
+    
+    
 }
 
